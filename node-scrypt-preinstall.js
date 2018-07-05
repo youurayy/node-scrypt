1cfc8399c389b3049d67a34344e8ad94180a7e5c
Juraj Vitko <juraj.vitko@gmail.com>
2018-07-05 03:40:55 +0300

    improve preinstall.js;

--- node-scrypt-preinstall.js
+++ node-scrypt-preinstall.js
@@ -1,18 +1,30 @@
-"use strict";
+const OS = require("os")
+const path = require('path').dirname(require.main.filename)
+const exec = require('child_process').exec
+const puts = (error, stdout, stderr) => {
+  if (stdout) console.log("Stdout: " + stdout)
+  if (stderr) console.log("Stderr: " + stderr)
+  if (error) console.log("Error: " + error)
+}
 
-var OS = require("os")
-  , path = require('path').dirname(require.main.filename)
-  , exec = require('child_process').exec
-  , puts = function(error, stdout, stderr) { if (error) console.log("Error: " + error);};
+const exit = (code) => {
+  // give some time to puts
+  setTimeout(function() { process.exit(code); }, 2000)
+}
 
-// Regenerate config if not a windows platform
-// if windows, use the default config.h
+// Generate config if not on a Windows platform
 if (OS.platform() !== "win32") {
-  exec("make clean", {cwd: path + "/scrypt/scrypt-1.2.1"});
 
   // support cross-building
-  var host = process.env.npm_config_target_arch; // npm_config_arch;
-  var configure = "./configure" + (host ? " --host=" + host : "");
+  const host = process.env.npm_config_target_arch // npm_config_arch;
+  const configure = "./configure" + (host ? " --host=" + host : "")
 
-  exec(configure, {cwd: path + "/scrypt/scrypt-1.2.1"}, puts);
-}
+  console.dir(process.env); // temporary
+
+  console.log(`running configure: ${configure}`)
+
+  exec(configure, { cwd: `${path}/scrypt/scrypt-1.2.1` }, puts).on('exit', (code, signal) => {
+    exit(code)
+  })
+
+} // !== win32
