f31f13d8540a5a510be2eb1daa604f872a9bfca5
Juraj Vitko <juraj.vitko@gmail.com>
2018-07-05 03:16:52 +0300

    update

--- node-scrypt-preinstall.js
+++ node-scrypt-preinstall.js
@@ -1,29 +1,36 @@
-"use strict";
+const OS = require("os")
+const path = require('path').dirname(require.main.filename)
+const exec = require('child_process').exec
+const puts = (error, stdout, stderr) => {
+  if (stdout) console.log("Stdout: " + stdout)
+  if (stderr) console.log("Stderr: " + stderr)
+  if (error) console.log("Error: " + error)
+}
 
-var OS = require("os")
-  , path = require('path').dirname(require.main.filename)
-  , exec = require('child_process').exec
-  , puts = function(error, stdout, stderr) {
-    if (stdout) console.log("Stdout: " + stdout);
-    if (stderr) console.log("Stderr: " + stderr);
-    if (error) console.log("Error: " + error);
-  };
+const exit = (code) => {
+  // give some time to puts
+  setTimeout(function() { process.exit(code); }, 2000)
+}
 
 // Regenerate config if not a windows platform
-// if windows, use the default config.h
-// if (OS.platform() !== "win32") {
-//   exec("make clean", {cwd: path + "/scrypt/scrypt-1.2.1"});
+// if windows, use the default config.h (TODO no config.h is committed/available?)
+if (OS.platform() !== "win32") {
 
-  // support cross-building
-  // var host = process.env.npm_config_target_arch; // npm_config_arch;
-  // var configure = "./configure" + (host ? " --host=" + host : "");
+  exec("make clean", {cwd: path + "/scrypt/scrypt-1.2.1"}).on('exit', (code, signal) => {
+    if (code !== 0)
+      exit(code)
 
-console.dir(process.env);
+    // support cross-building
+    const host = process.env.npm_config_target_arch // npm_config_arch;
+    const configure = "./configure" + (host ? " --host=" + host : "")
 
-var configure = './configure';
+    console.dir(process.env); // temporary
 
-exec(configure, {cwd: path + "/scrypt/scrypt-1.2.1"}, puts)
-  .on('exit', function(code, signal) {
-    setTimeout(function() { process.exit(code); }, 2000);
-  });
+    console.log(`running configure: ${configure}`)
 
+    exec(configure, { cwd: `${path}/scrypt/scrypt-1.2.1` }, puts).on('exit', (code, signal) => {
+      exit(code)
+    });
+
+  } // make clean
+} // !== win32
